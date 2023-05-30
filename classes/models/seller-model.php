9bf9e5123e06b44e485c4a032adb9d7fb6b08dfe
carlsylvan <carlsylvan@gmail.com>
2023-05-30 12:07:16 +0200

    seller model

--- classes/models/seller-model.php
+++ classes/models/seller-model.php
@@ -1,1 +1,12 @@
 <?php
+
+require_once './classes/db.php';
+
+class ProductModel extends DB {
+
+    protected $table = 'sellers';
+
+    public function getAllSellers() {
+        return $this->getAll($this->table);
+    }
+}
