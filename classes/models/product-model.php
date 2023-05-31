5076f79d9b6463adc58bc2a30846c9b45062ae01
carlsylvan <carlsylvan@gmail.com>
2023-05-31 14:38:37 +0200

    product model

--- classes/models/product-model.php
+++ classes/models/product-model.php
@@ -1,12 +1,35 @@
 <?php
 
 require_once './classes/db.php';
+require './classes/product.php';
+
 
 class ProductModel extends DB {
 
     protected $table = 'products';
 
-    public function getAllProducts() {
-        return $this->getAll($this->table);
+    public function getProductClass (array $productArray): array {
+        $products = [];
+        foreach ($productArray as $productSingle) {
+            $product = new Product (
+                $productSingle["id"],
+                $productSingle["title"],
+                $productSingle["description"],
+                $productSingle["price"],
+                $productSingle["seller_id"],
+                $productSingle["category_id"],
+                $productSingle["size_id"],
+                $productSingle["added"],
+                $productSingle["sold"],
+                $productSingle["color_id"],
+                $productSingle["brand_id"]
+            );
+            array_push($products, $product);
+        }
+        return $products;
+    }
+
+    public function getAllProducts(): array {
+        return $this->getProductClass($this->getAll($this->table));
     }
 }
