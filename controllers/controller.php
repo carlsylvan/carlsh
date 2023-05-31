90ef55dbe368cd4df775bd3553804253d288dafc
carlsylvan <carlsylvan@gmail.com>
2023-05-31 14:57:37 +0200

    index and controller class

---
+++ controllers/controller.php
@@ -0,0 +1,32 @@
+<?php
+
+class Controller {
+    private $model = null;
+    private $view = null;
+
+    private $table = "";
+
+    public function __construct($model, $view, $table) {
+        $this->model = $model;
+        $this->view = $view;
+        $this->table = $table;
+    }
+    private function checkUrl ($request) {
+        return explode("/", $request);
+    }
+    public function start($method, $request)
+    {   
+        $urlArray = $this->checkUrl($request);
+        // var_dump($urlArray);
+        switch ($request) {
+            case ($urlArray[2]==$this->table && count($urlArray)==3):
+                if($this->table == "sellers") {
+                    $this->view->outputAll($this->table,$this->model->getAllSellers());
+                }
+                break;
+            case ($urlArray[3] && count($urlArray)==4):
+                $this->view->outputAll($this->table,$this->model->getOneSeller((int)$urlArray[3]));
+                break;
+        }
+    }
+}
--- index.php
+++ index.php
@@ -1,21 +1,32 @@
 <?php
 
 require 'classes/models/product-model.php';
-require 'classes/views/product-view.php';
+// require 'classes/views/product-view.php';
 
 require 'classes/models/seller-model.php';
-require 'classes/views/seller-view.php';
+// require 'classes/views/seller-view.php';
 
 $pdo = require 'partials/connect.php';
 
+$request = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+$sellerModel = new SellerModel();
+$productModel = new ProuctModel();
+
+$jsonApi = new JsonApi();
+
+$sellerController = new Controller($sellerModel, $jsonApi, "sellers");
+$sellerController->start($method, $request);
 
 
-$productModel = new ProductModel($pdo);
-$productView = new ProductView();
 
-$sellerModel = new SellerModel($pdo);
-$sellerView = new SellerView();
+// $productModel = new ProductModel($pdo);
+// $productView = new ProductView();
 
-$productView->renderAllProducts($productModel->getAllProducts());
+// $sellerModel = new SellerModel($pdo);
+// $sellerView = new SellerView();
 
-$sellerView->renderAllSellers($sellerModel->getAllSellers());
+// $productView->renderAllProducts($productModel->getAllProducts());
+
+// $sellerView->renderAllSellers($sellerModel->getAllSellers());
