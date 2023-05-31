ecae11eb8fa9a09917e0d790c7e8d71a451b80ff
carlsylvan <carlsylvan@gmail.com>
2023-05-31 14:27:14 +0200

    seller class

--- classes/seller.php
+++ classes/seller.php
@@ -1,1 +1,17 @@
 <?php
+
+class Seller {
+    public int $id = 0;
+    public string $first_name = "";
+    public string $last_name = "";
+    public string $phone = "";
+    public string $email = "";
+
+    function __construct($id, $first_name, $last_name, $phone, $email) {
+        $this->id = $id;
+        $this->first_name = $first_name;
+        $this->last_name = $last_name;
+        $this->phone = $phone;
+        $this->email = $email;
+    }
+}
