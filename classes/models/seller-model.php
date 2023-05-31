467d37e3d5891e4cf1a8eddf1cd99c2b9a2d0b69
carlsylvan <carlsylvan@gmail.com>
2023-05-31 14:47:17 +0200

    seller model and json api view

--- classes/models/seller-model.php
+++ classes/models/seller-model.php
@@ -1,12 +1,29 @@
 <?php
 
 require_once './classes/db.php';
+require './classes/seller.php';
+
 
 class SellerModel extends DB {
 
     protected $table = 'sellers';
 
-    public function getAllSellers() {
-        return $this->getAll($this->table);
+    public function getSellerClass (array $sellerArray): array {
+        $sellers = [];
+        foreach ($sellerArray as $sellerSingle) {
+            $product = new Product (
+                $sellerSingle["id"],
+                $sellerSingle["first_name"],
+                $sellerSingle["last_name"],
+                $sellerSingle["phone"],
+                $sellerSingle["email"],
+            );
+            array_push($sellers, $seller);
+        }
+        return $sellers;
+    }
+
+    public function getAllSellers(): array {
+        return $this->getSellerClass($this->getAll($this->table));
     }
 }
---
+++ classes/views/json-api.php
@@ -0,0 +1,12 @@
+<?php
+
+class jsonApi {
+    public function outputJson(string $single, array $array) {
+        $json = [
+            "$result_count" => count($array),
+            "$result" => $array
+        ];
+        header("Content-Type: application/json");
+        echo json_encode($json);
+    }
+}
