5d9a2b6c85e88db50ececfa884d359f853787fd5
carlsylvan <carlsylvan@gmail.com>
2023-06-12 22:07:57 +0200

    json api

--- classes/views/json-api.php
+++ classes/views/json-api.php
@@ -1,12 +1,14 @@
 <?php
 
 class JsonApi {
-    public function outputJson ($input) {
-        if($input== "error") {
-            echo "Route is not found";
-        }
-        else {
-            $json = [            
+    public function outputJson($input)
+    {
+        if ($input == "error") {
+            http_response_code(404);
+            echo json_encode(["error" => "Route not found"]);
+        } else {
+            http_response_code(200);
+            $json = [
                 "result_count" => count($input),
                 "result" => $input
             ];
